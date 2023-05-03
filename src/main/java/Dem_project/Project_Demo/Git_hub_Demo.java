082880e83779d9f2f2b02bf8b5938c93fbf95cf4
POLICE <POLICE@LAPTOP-7IIH805G>
2023-05-03 16:22:35 +0530

    First commit

---
+++ src/main/java/Dem_project/Project_Demo/Git_hub_Demo.java
@@ -0,0 +1,17 @@
+package Dem_project.Project_Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Git_hub_Demo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\POLICE\\.m2\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+       
+		System.out.println("I am from Allstate india");
+		
+	}
+
+}
